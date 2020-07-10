1abf4777c8d362e745257374d8679adb35be411a
floury-fabio <floury-fabio@hotmail.fr>
2020-07-10 19:07:09 +0200

    uniformize conditional rendering

--- src/components/playerList.jsx
+++ src/components/playerList.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import Player from 'components/player';
 
 const PlayerList = ({ players }) => {
@@ -10,23 +12,39 @@ const PlayerList = ({ players }) => {
     return playersList;
   };
 
+  const setupPlayersListOrAddInvitation = () => {
+    let content;
+    if (players === null) {
+      content = (
+        <div className="container d-flex flex-column justify-content-center align-items-center my-5">
+          <p>It seems you don&apost have players yet, let&aposs add some of them now</p>
+          <Link to="/" className="btn btn-primary">Add Players</Link>
+        </div>
+      );
+    } else {
+      content = (
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Name</th>
+              <th scope="col">Jersey</th>
+              <th scope="col">Position</th>
+              <th scope="col">Height</th>
+              <th scope="col">Weight</th>
+              <th scope="col">Email</th>
+              <th scope="col">Phone</th>
+            </tr>
+          </thead>
+          <tbody>
+            {setList()}
+          </tbody>
+        </table>
+      );
+    }
+    return content;
+  };
   return (
-    <table className="table">
-      <thead>
-        <tr>
-          <th scope="col">Name</th>
-          <th scope="col">Jersey</th>
-          <th scope="col">Position</th>
-          <th scope="col">Height</th>
-          <th scope="col">Weight</th>
-          <th scope="col">Email</th>
-          <th scope="col">Phone</th>
-        </tr>
-      </thead>
-      <tbody>
-        {setList()}
-      </tbody>
-    </table>
+    { setupPlayersListOrAddInvitation }
   );
 };
 
