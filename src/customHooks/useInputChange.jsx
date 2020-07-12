7009cf7f266cd8850d777fc808d18693eabb551b
floury-fabio <floury-fabio@hotmail.fr>
2020-07-12 15:43:36 +0200

    add use input change

---
+++ src/customHooks/useInputChange.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+const useInputChange = () => {
+  const [input, setInput] = useState({});
+
+  const handleInputChange = (e) => setInput({
+    ...input,
+    [e.currentTarget.name]: e.currentTarget.value,
+  });
+
+  return [input, handleInputChange];
+};
+export default useInputChange;
