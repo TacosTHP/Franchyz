56c87891eff84538c87c07e1baebd2d88b70f15a
floury-fabio <floury-fabio@hotmail.fr>
2020-07-12 10:32:10 +0200

    rework dashboardAdminTabs

--- src/components/dashboardAdminTabs.jsx
+++ src/components/dashboardAdminTabs.jsx
@@ -1,39 +1,47 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Tabs } from 'antd';
-import ClubInformations from './clubInformations.jsx';
-import ClubEdit from './clubEdit.jsx';
-import TeamList from './teamList.jsx';
+import ClubInformations from './clubInformations';
+import ClubEdit from './clubEdit';
+import TeamList from './teamList';
 
 const DashboardAdminTabs = ({club}) => {
-  const {TabPane} = Tabs;
+  const { TabPane } = Tabs;
 
-  console.log("clubtest" + club.id)
-  return(
-    <div className='container rounded mt-5' style={{backgroundColor: '#E8E7E7'}}>
-      <br></br>
-      <div className='card-container'>
-        <Tabs type='card'>
-          <TabPane tab='Your Club' key='1'>
-            <ClubInformations club={club} />
-          </TabPane>
-          { club.id === undefined ? "" : 
-          <TabPane tab='Edit Club' key='2'>
-          <ClubEdit club={club} />
-          </TabPane>
-          }
-          <TabPane tab='Your teams' key='3'>
-            <TeamList teams={club.teams} />
-            <Link to='/newTeam'>
-              <button type='button' className='btn btn-primary ml-4 mt-4'>
-                Add new Team
-              </button>
-            </Link>
-          </TabPane>
-        </Tabs>
-      </div>
-      <br></br>
-    </div>
+  const setupDashboardAdminTabs = () => {
+    let content;
+    if (club !== undefined) {
+      content = (
+        <div className="container rounded mt-5" style={{ backgroundColor: '#E8E7E7' }}>
+          <br />
+          <div className="card-container">
+            <Tabs type="card">
+              <TabPane tab="Your Club" key="1">
+                <ClubInformations club={club} />
+              </TabPane>
+              <TabPane tab="Edit Club" key="2">
+                <ClubEdit club={club} />
+              </TabPane>
+              <TabPane tab="Your teams" key="3">
+                <TeamList teams={club.teams} />
+                <Link to="/newTeam">
+                  <button type="button" className="btn btn-primary ml-4 mt-4">
+                    Add new Team
+                  </button>
+                </Link>
+              </TabPane>
+            </Tabs>
+          </div>
+          <br />
+        </div>
+      );
+    }
+    return content;
+  };
+  return (
+    <>
+      {setupDashboardAdminTabs()}
+    </>
   );
 };
 
