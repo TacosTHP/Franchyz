c8f1b741774f66d53be209da99d5ecf314522394
floury-fabio <floury-fabio@hotmail.fr>
2020-07-12 10:31:41 +0200

    rework adminCoachDashboardPage

--- src/pages/adminCoachDashboardPage.jsx
+++ src/pages/adminCoachDashboardPage.jsx
@@ -1,66 +1,57 @@
-import React, {useEffect, useState} from "react";
-import "../styles/form.scss";
-import {useSelector} from "react-redux";
-import {Link} from "react-router-dom";
-import * as clubAPI from "services/clubAPI.jsx";
-import DashboardAdminTabs from "components/dashboardAdminTabs.jsx";
-import Calendar from 'components/calendar'
+import React, { useEffect, useState } from 'react';
+import '../styles/form.scss';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import * as clubAPI from 'services/clubAPI';
+import DashboardAdminTabs from 'components/dashboardAdminTabs';
+import Calendar from 'components/calendar';
 
-function AdminCoachDashboardPage() {
+const AdminCoachDashboardPage = () => {
   const myClubId = useSelector((state) => state.userReducer.clubId);
-
-  const [club, setClub] = useState("");
-
-  useEffect(() => {
-    loadClub();
-  }, []);
+  const [club, setClub] = useState();
 
   const loadClub = async () => {
     const response = await clubAPI.getClub(myClubId);
+    console.log(response)
     setClub(response);
-  }
+  };
+
+  const setupPageOrInvitation = () => {
+    let content;
+    console.log(myClubId, 'dwe')
+    if (myClubId === undefined) {
+      content = (
+        <>
+          <div>
+            <h1> Welcome to FRANCHYZ </h1>
+            <h4> You just created an acccount for your sport club. </h4>
+            <h4> Start creating a club and adding a team before you start creating events. </h4>
+          </div>
+          <Link to="/newClub">
+            <button type="button" className="btn btn-primary mt-4 ml-3"> Create club </button>
+          </Link>
+        </>
+      );
+    } else {
+      content = (
+        <div>
+          <h1>Dashboard FRANCHYZ</h1>
+          <h4> Your clublllll  </h4>
+          <DashboardAdminTabs club={club} />
+        </div>
+      );
+    }
+    return content;
+  };
+
+  useEffect(() => { loadClub(); }, []);
 
   return (
     <>
-      <div className="text-center mt-5">
-        {myClubId === null ? (
-          <div>
-            <h1>Welcome to FRANCHYZ</h1>
-            <h4>You just created an acccount for your sport club.</h4>
-            <h4>
-              Start creating a club and adding a team before you start creating events.
-            </h4>
-          </div>
-        ) : (
-          <div>
-            <h1>Dashboard FRANCHYZ</h1>
-            <h4>
-              Start creating a club and adding a team before you start creating events.
-            </h4>
-          </div>
-        )}
-
-        {myClubId === null ? (
-          <Link to="/newClub">
-            <button type="button" className="btn btn-primary mt-4 ml-3">
-              Create club
-            </button>
-          </Link>
-        ) : (
-          <Link to="/newEvent">
-            <button type="button" className="btn btn-primary mt-4 ml-4">
-              Create event
-            </button>
-          </Link>
-        )}
-      </div>
-
-      <DashboardAdminTabs club={club} />
-        <Calendar/>
-
+      {setupPageOrInvitation()}
       <br />
     </>
   );
-}
+};
 
 export default AdminCoachDashboardPage;
