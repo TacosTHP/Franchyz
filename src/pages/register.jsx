7a1b6ad72e499f0d8ca9d2dce2fdc2798d78ccdd
floury-fabio <floury-fabio@hotmail.fr>
2020-07-14 11:00:29 +0200

    setup register page for call registerFrom

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,26 +1,34 @@
 import React, { useEffect, useState } from 'react';
-import { logup } from 'redux/middlewares/authMiddlewares';
-import { useSelector, useDispatch } from 'react-redux';
-import { Redirect } from 'react-router-dom'
-import * as clubAPI from '../services/clubAPI.jsx';
-import * as teamAPI from '../services/teamAPI.jsx';
-import '../styles/form.scss';
-import { message} from 'antd';
-import { RegisterForm } from 'components/RegisterForm'
-
+import * as clubAPI from 'services/clubAPI';
+import RegisterForm from 'components/RegisterForm';
 
 const Register = () => {
+  const [clubs, setClubs] = useState(null);
 
-  const [clubs, setClubs] = useState()
+  const setupLoadingClubs = () => {
+    let content;
+    if (clubs !== null) {
+      content = <RegisterForm clubs={clubs} />;
+    } else {
+      content = <p> loading </p>;
+    }
+    return content;
+  };
 
-  useEffect( async () => {
-    const response = await clubAPI.getClubs()
-    setClubs(response);
-  })
+  useEffect(() => {
+    const loadClubs = async () => {
+      const response = await clubAPI.getClubs();
+      setClubs(response);
+    };
+    loadClubs();
+  }, []);
+
   return (
-    <RegisterForm clubs={clubs} />
-
-  )
+    <>
+      <h3> Register ! </h3>
+      {setupLoadingClubs()}
+    </>
+  );
 };
 
 export default Register;
