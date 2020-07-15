8f5a2829c41a1c98c62083562c97d2e52d96f8ac
Dim <dimitri.kiavue@gmail.com>
2020-07-15 17:52:28 +0200

    [ADD] FullCalendar Builder helper

--- src/components/calendar.jsx
+++ src/components/calendar.jsx
@@ -1,125 +1,74 @@
-import React, { useState, useEffect } from 'react'
-import { useHistory } from "react-router-dom";
-import moment from "moment";
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import PropTypes from 'prop-types';
 
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
 
-import FullCalendar from '@fullcalendar/react'
-import dayGridPlugin from "@fullcalendar/daygrid";
-import timeGridPlugin from "@fullcalendar/timegrid";
-import interactionPlugin from "@fullcalendar/interaction";
-import {useSelector } from 'react-redux'
+import '../styles/calendar.scss';
 
-
-import * as EventsAPI from 'services/eventsAPI';
-import * as clubAPI from "services/clubAPI.jsx";
-
-import '../styles/calendar.scss'
-
-function Calendar() {
-  const [games, setGames] = useState([])
-  const [practices, setPractices] = useState([])
-  const userType = useSelector(state => state.authReducer.userType);
-  const club_id = useSelector(state => state.userReducer.clubId)
-  const team_id = useSelector(state => state.userReducer.teamId)
-  const user_id = useSelector(state => state.userReducer.id)
+const Calendar = ({ attendances }) => {
+  const userType = useSelector((state) => state.authReducer.userType);
   const history = useHistory();
 
-  const addEventManually = arg => {
-    setGames([...games, {title: "Event Now", start: arg.date}])
-  }
-
   const goToEventNew = () => {
-    if (userType === "coach"){
-      history.push('/newEvent')
+    if (userType === 'coach') {
+      history.push('/newEvent');
     }
-  }
+  };
 
-  const getGames =() => {
-    EventsAPI.getAttendedGames(user_id, club_id, team_id)
-    .then(response => {if (response.length === undefined) {
-      console.log("no Attended games!");
+  const setupElements = () => {
+    let content;
+    if (attendances !== undefined) {
+      content = (
+        <>
+          <FullCalendar
+            plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+            defaultView="timeGridDay"
+            themeSystem="bootstrap"
+            height="auto"
+            header={{
+              left: 'prev,next today',
+              center: 'title',
+              right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek',
+            }}
+            allDaySlot={false}
+            firstDay={1}
+            locale="en"
+            timeZone="UTC"
+            buttonText={{
+              today: 'Today',
+              day: 'Daily',
+              month: 'Monthly',
+              week: 'Weekly',
+            }}
+            events={attendances}
+            dateClick={goToEventNew}
+          />
+        </>
+      );
     } else {
-      response.map(game => setGames([...games, {
-        title: `GAME ${game.title}`,
-        start: game.start,
-        color: game.color,
-        allDay: false
-      }]))
-    }})
-  }
-
-  const getPractices =() => {
-    EventsAPI.getAttendedPractices(user_id, club_id, team_id)
-    .then(response => {
-      response.map(practice => setPractices([...practices, {
-        title: `PRACTICE ${practice.title}`,
-        start: practice.start,
-        color: practice.color,
-        allDay: false
-      }]));
-    })
-
-  }
-  
-  
-  const retrieveGames = () => {
-  if (userType === "player") {
-    getGames ()
-  } else {
-     clubAPI.getClub(club_id)
-     .then(response => {if (response.games === undefined) {
-      console.log("no games!");
-    } else {
-      console.log(response.games);
-      setGames(response.games)
-    }})
-  }
- }
- const retrievePractices = () => {
-  if (userType === "player") {
-    getPractices ()
-  } else {
-      clubAPI.getClub(club_id)
-     .then(response => {if (response.practices === undefined) {
-      console.log("no practices!");
-      console.log("response" + response);
-     } else {
-      setPractices(response.practices)
-    }})
-  }
- }
-
-  useEffect(() => {retrievePractices() }, [])
-   useEffect(() => {retrieveGames()} , [])
-const handleEvent = () => {
-  console.log("event")
-}
+      content = (
+        <div>
+          <h2>No events booked yet mate.......</h2>
+        </div>
+      );
+    }
+    return content;
+  };
 
   return (
-    <FullCalendar
-      plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-      defaultView="dayGridMonth"
-      themeSystem="bootstrap"
-      height="auto"
-      header={{
-              left: "prev,next today",
-              center: "title",
-              right: "dayGridMonth,timeGridWeek,timeGridDay,listWeek"
-            }}
-      allDaySlot={false}
-      firstDay={1}
-      locale= "fr"
-      timeZone="UTC"
-      buttonText= {{
-        today:"Aujourd'hui",
-        day: 'Jour',
-        month: 'Mois',
-        week: 'Semaine',
-      }}
-      eventSources={[practices, games]}
-      dateClick={goToEventNew}
-    />
-  )
-}
+    <div>
+      { setupElements() }
+    </div>
+  );
+};
 
-export default Calendar
+export default Calendar;
+
+Calendar.propTypes = {
+  attendances: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
