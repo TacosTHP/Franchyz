2041eea87486110222e16cc1ed7dd6795c1c01dc
floury-fabio <floury-fabio@hotmail.fr>
2020-07-15 11:53:20 +0200

    add select teams

--- src/components/EventNewForm.jsx
+++ src/components/EventNewForm.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import useInputChange from 'customHooks/useInputChange';
+import PropTypes from 'prop-types';
 
-const EventNewForm = () => {
+const EventNewForm = ({ teams }) => {
+  const [input, handleInputChange] = useInputChange();
+
   return (
-    <>
-    </>
+    <div className="container">
+      <select id="teamId" name="teamId" className="form-control" defaultValue="" onChange={handleInputChange}>
+        {teams.map((team) => (<option key={`${team.name} ${team.id}`} value={team.id}>{team.title}</option>))}
+      </select>
+    </div>
   );
 };
 
 export default EventNewForm;
+
+EventNewForm.propTypes = {
+  teams: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
