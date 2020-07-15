fc9c09c340fa120ae58c751300c28de10fb07e07
floury-fabio <floury-fabio@hotmail.fr>
2020-07-15 11:11:41 +0200

    setup Event new Page

---
+++ src/pages/EventNewPage.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import EventNewForm from 'components/EventNewForm';
+import * as teamAPI from 'services/teamAPI';
+
+const EventNewPage = () => {
+  const [teams, setTeams] = useState(null);
+  const clubId = useSelector((state) => state.userReducer.clubId);
+
+  useEffect(() => {
+    const loadTeams = async () => {
+      const response = await teamAPI.getTeams(clubId);
+      setTeams(response);
+    };
+    loadTeams();
+  }, []);
+
+  return (
+    <>
+      <EventNewForm teams={teams} />
+    </>
+  );
+};
+
+export default EventNewPage;
