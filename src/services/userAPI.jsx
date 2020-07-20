9c0079a6f15ca1bf8a0e76ed04a4875c262bdf59
Dim <dimitri.kiavue@gmail.com>
2020-07-20 13:28:30 +0200

    [UPDATE] Clean userAPI

--- src/services/userAPI.jsx
+++ src/services/userAPI.jsx
@@ -19,26 +19,24 @@ const getPlayer = (clubId, teamId, playerId) => {
     .then((response) => response.json());
 };
 
-const playerUpdate = ({clubId, teamId, playerId, data}) => {
+const playerUpdate = (clubId, teamId, playerId, data) => {
+  const baseURL = process.env.REACT_APP_API_URL;
+  const endUrl = `/clubs/${clubId}/teams/${teamId}/players/${playerId}`;
+  const url = baseURL + endUrl;
 
-  let baseURL = process.env.REACT_APP_API_URL;
-  let endUrl = `/clubs/${clubId}/teams/${teamId}/players/${playerId}`;
-  let url = baseURL + endUrl;
-
-  let headers = {
+  const headers = {
     'Content-Type': 'application/json',
     Authorization: Cookies.get('token'),
   };
 
-  let request = {
+  const request = {
     method: 'PUT',
-    headers: headers,
-    body: JSON.stringify(data)
+    headers,
+    body: JSON.stringify(data),
   };
 
   return fetch(url, request)
-    .then(response => response.json())
-    .then(response => {return response})
+    .then((response) => response.json());
 };
 
 export { getPlayer, playerUpdate };
