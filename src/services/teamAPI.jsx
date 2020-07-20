7f5dbeb44d68ab8115adabc2742ddf076fb9e8ea
Dim <dimitri.kiavue@gmail.com>
2020-07-20 13:56:09 +0200

    [UPDATE] Clean teamAPI file

--- src/services/teamAPI.jsx
+++ src/services/teamAPI.jsx
@@ -32,42 +32,9 @@ const getTeam = ({ clubId, teamId }) => {
     .then((response) => response.json());
 };
 
-function getAllTeams(id) {
-  let baseURL = process.env.REACT_APP_API_URL;
-  let endUrl = `/clubs/${id}/teams.json`;
-  let url = baseURL + endUrl;
-
-  return fetch(url, {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-    .then((response) => response.json())
-    .then((response) => {
-      return response;
-    });
-}
-function getTeamsOfClub(id) {
-  let baseURL = process.env.REACT_APP_API_URL;
-  let endUrl = `/clubs/${id}/teams.json`;
-  let url = baseURL + endUrl;
-
-  let headers = {
-    "Content-Type": "application/json",
-  };
-
-  let request = {
-    headers: headers,
-  };
-
-  return fetch(url, request)
-    .then((response) => response.json())
-    .then((response) => {
-      return response;
-    });
-}
-
-function createTeam(team, creatorId, coachId, clubId) {
+const createTeam = ({
+  team, creatorId, coachId, clubId,
+}) => {
   const data = {
     title: team,
     creator_id: creatorId,
@@ -75,19 +42,17 @@ function createTeam(team, creatorId, coachId, clubId) {
     coach_id: coachId,
   };
 
-  let baseURL = process.env.REACT_APP_API_URL;
-  let endUrl = `/clubs/${clubId}/teams.json`;
-  let url = baseURL + endUrl;
+  const baseURL = process.env.REACT_APP_API_URL;
+  const endUrl = `/clubs/${clubId}/teams.json`;
+  const url = baseURL + endUrl;
 
   return fetch(url, {
-    method: "post",
+    method: 'post',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify(data),
-  }).then((response) => {
-    return response;
-  });
-}
-
-export { createTeam, getAllTeams, getTeamsOfClub, getTeam, getTeams };
+  })
+    .then((response) => response.json());
+};
+export { createTeam, getTeam, getTeams };
