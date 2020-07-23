2fc0ad3980f509ab26d4a030e793a8d9fbfa3eff
Dim <dimitri.kiavue@gmail.com>
2020-07-23 16:56:54 +0200

    [UPDATE] Clean Carousel

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -1,51 +1,47 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
-import '../styles/carousel.scss'
+import { Link } from 'react-router-dom';
+import 'styles/carousel.scss';
 
-function Carousel() {
-
-
-  return(
-    <>
-      <div id="carouselExampleSlidesOnly" className="carousel slide h-50" data-ride="carousel">
-        <div className="carousel-inner">
-          <div className="carousel-item active">
-            <div className="hero-image-1">
-              <div className="hero-text-1">
-                <h1 style={{color: "white"}}>FRANCHYZ</h1>
-                <p>TEAM MANAGEMENT THE EASY WAY</p>
-                <Link to="/register">
-                  <button type="button" className="btn btn-primary"> Register now</button>
-                </Link>
-              </div>
+const Carousel = () => (
+  <>
+    <div id="carouselExampleSlidesOnly" className="carousel slide h-50" data-ride="carousel">
+      <div className="carousel-inner">
+        <div className="carousel-item active">
+          <div className="hero-image-1">
+            <div className="hero-text-1">
+              <h1 style={{ color: 'white' }}>FRANCHYZ</h1>
+              <p>TEAM MANAGEMENT THE EASY WAY</p>
+              <Link to="/register">
+                <button type="button" className="btn btn-primary"> Register now</button>
+              </Link>
             </div>
           </div>
-          <div className="carousel-item">
-            <div className="hero-image-2">
-              <div className="hero-text-2">
-                <h1 style={{color: "white"}}>FRANCHYZ</h1>
-                <p>TEAM MANAGEMENT THE EASY WAY</p>
-                <Link to="/register">
-                  <button type="button" className="btn btn-primary"> Register now</button>
-                </Link>
-              </div>
+        </div>
+        <div className="carousel-item">
+          <div className="hero-image-2">
+            <div className="hero-text-2">
+              <h1 style={{ color: 'white' }}>FRANCHYZ</h1>
+              <p>TEAM MANAGEMENT THE EASY WAY</p>
+              <Link to="/register">
+                <button type="button" className="btn btn-primary"> Register now</button>
+              </Link>
             </div>
           </div>
-          <div className="carousel-item">
-            <div className="hero-image-3">
-              <div className="hero-text-3">
-                <h1 style={{color: "white"}}>FRANCHYZ</h1>
-                <p>TEAM MANAGEMENT THE EASY WAY</p>
-                <Link to="/register">
-                  <button type="button" className="btn btn-primary"> Register now</button>
-                </Link>
-              </div>
+        </div>
+        <div className="carousel-item">
+          <div className="hero-image-3">
+            <div className="hero-text-3">
+              <h1 style={{ color: 'white' }}>FRANCHYZ</h1>
+              <p>TEAM MANAGEMENT THE EASY WAY</p>
+              <Link to="/register">
+                <button type="button" className="btn btn-primary"> Register now</button>
+              </Link>
             </div>
           </div>
         </div>
       </div>
-    </>
-  )
-}
+    </div>
+  </>
+);
 
-export default Carousel
+export default Carousel;
