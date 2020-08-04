ed656660a8447162518d5a887da5af189ed705f5
Dim <dimitri.kiavue@gmail.com>
2020-08-04 13:52:04 +0200

    [UPDATE] clean footer.jsx

--- src/components/layouts/footer.jsx
+++ src/components/layouts/footer.jsx
@@ -1,13 +1,11 @@
-import React from "react";
-import "../../styles/footer.scss";
+import React from 'react';
+import 'styles/footer.scss';
 
-function Footer() {
-	return (
-		<div class="footer bg-dark py-5">
-			<p className="small text-center text-muted">
-				Copyright &copy; 2020 - FRANCHYZ
-			</p>
-		</div>
-	);
-}
+const Footer = () => (
+  <div className="footer bg-dark py-5">
+    <p className="small text-center text-muted">
+      Copyright &copy; 2020 - FRANCHYZ
+    </p>
+  </div>
+);
 export default Footer;
