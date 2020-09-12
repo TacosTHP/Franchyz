d4fd69f26c558d4ca4d08860dbefe1b530d4f911
Dim <dimitri.kiavue@gmail.com>
2020-09-12 14:46:45 +0200

    [ADD] StatisticsBandeau

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import HomeNavbar from 'components/layouts/HomeNavbar';
 import Carousel from 'components/Carousel';
+import StatisticsBandeau from 'components/StatisticsBandeau';
 
 const Home = () => (
   <>
     <HomeNavbar />
     <Carousel />
-    <Carousel />
+    <StatisticsBandeau />
     <Carousel />
   </>
 );
