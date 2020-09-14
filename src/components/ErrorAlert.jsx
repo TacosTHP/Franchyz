8e59dd1745a8390589832cbcd9e56645316c18b0
floury-fabio <floury-fabio@hotmail.fr>
2020-09-14 13:48:15 +0200

    add reset in errorMessage

--- src/components/ErrorAlert.jsx
+++ src/components/ErrorAlert.jsx
@@ -1,12 +1,15 @@
 import React, { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { resetError } from 'redux/actions/authActions';
 import { message } from 'antd';
 
 const ErrorAlert = () => {
   const errorMessage = useSelector((state) => state.authReducer.error);
+  const dispatch = useDispatch();
   useEffect(() => {
     if (errorMessage !== null) {
       message.error(errorMessage, 2.5);
+      dispatch(resetError());
     }
   }, [errorMessage]);
   return (
