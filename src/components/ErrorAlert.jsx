279339953c1f393104856235e8de49ae2837bf23
floury-fabio <floury-fabio@hotmail.fr>
2020-09-14 16:38:00 +0200

    error -> errorMessage

--- src/components/ErrorAlert.jsx
+++ src/components/ErrorAlert.jsx
@@ -1,15 +1,15 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { resetError } from 'redux/actions/authActions';
+import { resetErrorMessage } from 'redux/actions/authActions';
 import { message } from 'antd';
 
 const ErrorAlert = () => {
-  const errorMessage = useSelector((state) => state.authReducer.error);
+  const errorMessage = useSelector((state) => state.authReducer.errorMessage);
   const dispatch = useDispatch();
   useEffect(() => {
     if (errorMessage !== null) {
       message.error(errorMessage, 2.5);
-      dispatch(resetError());
+      dispatch(resetErrorMessage());
     }
   }, [errorMessage]);
   return (
