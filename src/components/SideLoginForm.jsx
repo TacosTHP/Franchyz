7e67ae4f796c73010476712cac6658b3293145f1
Dim <dimitri.kiavue@gmail.com>
2020-09-15 15:45:29 +0200

    [UPDATE] SideLoginForm Component ONLY

--- src/components/SideLoginForm.jsx
+++ src/components/SideLoginForm.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
+import { useHistory, Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
+
 import { message } from 'antd';
 
 import useInputChange from 'customHooks/useInputChange';
@@ -36,33 +37,50 @@ const SideLoginForm = () => {
   }, [error]);
 
   return (
-    <form className="rounded bg-dark p-3" onSubmit={submit}>
-      <div className="form-group">
-        <label htmlFor="email">
-          You are :
-          <select id="type" name="type" className="form-control" defaultValue="" onChange={handleInputChange} required>
-            <option value="" disabled hidden>Choose an option</option>
-            <option value="coach">Coach</option>
-            <option value="player">Player</option>
-          </select>
-        </label>
-      </div>
+    <div className="d-flex flex-column justify-content-center align-items-center py-4" id="form-wrapper">
+      <h3 className="text-primary">Log in to get Instant Access !</h3>
+      <div className="px-3 my-3">
+        <form className="rounded bg-dark p-3" onSubmit={submit}>
+          <div className="form-group">
+            <label htmlFor="email">
+              You are :
+              <select id="type" name="type" className="form-control" defaultValue="" onChange={handleInputChange} required>
+                <option value="" disabled hidden>Choose an option</option>
+                <option value="coach">Coach</option>
+                <option value="player">Player</option>
+              </select>
+            </label>
+          </div>
 
-      <div className="form-group">
-        <label htmlFor="email">
-          Email :
-          <input id="email" name="email" type="email" className="form-control" placeholder="Enter email" onChange={handleInputChange} />
-        </label>
-      </div>
+          <div className="form-group">
+            <label htmlFor="email">
+              Email :
+              <input id="email" name="email" type="email" className="form-control" placeholder="Enter email" onChange={handleInputChange} />
+            </label>
+          </div>
 
-      <div className="form-group">
-        <label htmlFor="pwd">
-          Password :
-          <input id="password" name="password" type="password" className="form-control" placeholder="Enter password" onChange={handleInputChange} />
-        </label>
+          <div className="form-group">
+            <label htmlFor="pwd">
+              Password :
+              <input id="password" name="password" type="password" className="form-control" placeholder="Enter password" onChange={handleInputChange} />
+            </label>
+          </div>
+          <button type="submit" className="btn btn-primary w-100">Submit</button>
+        </form>
       </div>
-      <button type="submit" className="btn btn-primary w-100">Submit</button>
-    </form>
+      <div className="d-flex flex-column align-items-center">
+        <Link to="/">
+          <p className="text-primary">
+            Forgot password?
+          </p>
+        </Link>
+        <Link to="/register">
+          <p className="text-primary">
+            Not registered yet ? Create your account here !
+          </p>
+        </Link>
+      </div>
+    </div>
   );
 };
 
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,38 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 import SideLoginForm from 'components/SideLoginForm';
-
-import 'styles/form.scss';
 
 const LoginPage = () => (
   <>
     <div className="d-flex">
-      <div
-        className="d-flex flex-column justify-content-center align-items-center py-4"
-        style={{
-          width: '40%',
-          backgroundColor: '#000',
-        }}
-      >
-        <h3 className="text-primary">Log in to get Instant Access !</h3>
-        <div className="px-3 my-3">
-          <SideLoginForm />
-        </div>
-        <div className="d-flex flex-column align-items-center">
-          <Link to="/">
-            <p className="text-primary">
-              Forgot password?
-            </p>
-          </Link>
-          <Link to="/register">
-            <p className="text-primary">
-              Not registered yet ? Create your account here !
-            </p>
-          </Link>
-        </div>
-      </div>
-      <div className="hero-image-login" style={{ width: '60%' }} />
+      <SideLoginForm />
+      <div className="hero-image-login" />
     </div>
   </>
 );
