9ac83d7811dc2eedc30c4fe58d61db6ab84e9381
floury-fabio <floury-fabio@hotmail.fr>
2020-09-15 17:56:09 +0200

    add redirect and error management in createClubMiddleware

--- src/redux/middlewares/clubsMiddlewares.jsx
+++ src/redux/middlewares/clubsMiddlewares.jsx
@@ -1,11 +1,12 @@
 import * as clubAPI from 'services/clubAPI';
-import { loginRequest, loginFailure } from 'redux/actions/authActions';
+import { request, requestFailure, connect } from 'redux/actions/authActions';
 import { updateClubId } from 'redux/actions/userActions';
 import { updateUserInfo } from 'helpers/reducersHelpers';
+import { setupErrorsMessage } from 'helpers/misc';
 
 const createClub = (args) => async (dispatch) => {
   try {
-    dispatch(loginRequest());
+    dispatch(request());
     const response = await clubAPI.createClub(args);
     const body = await response.json();
 
@@ -15,8 +16,10 @@ const createClub = (args) => async (dispatch) => {
 
     updateUserInfo({ clubId: body.id });
     dispatch(updateClubId({ clubId: body.id }));
-  } catch (error) {
-    console.log(error)
+    dispatch(connect('dashboardAdmin'));
+  } catch (errors) {
+    dispatch(requestFailure(setupErrorsMessage(errors)));
+    dispatch(connect('/'));
   }
 };
 
