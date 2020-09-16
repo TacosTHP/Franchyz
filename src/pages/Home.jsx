3a6053a336a10fb6e970694c1426bf3e21efb588
Dim <dimitri.kiavue@gmail.com>
2020-09-16 10:43:26 +0200

    [ADD] Helpers for generic content

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,65 +7,40 @@ import PrimaryCTABandeau from 'components/PrimaryCTABandeau';
 import Testimonials from 'components/Testimonials';
 import Footer from 'components/layouts/Footer';
 
-import image from 'assets/action-american-football-athletes-field-260697.jpg';
-import cedric from 'assets/cedric.jpeg';
-import aude from 'assets/aude.jpeg';
-import nadia from 'assets/nadia.jpg';
+import * as content from 'helpers/contentHelpers';
 
-const Home = () => {
-  const title = 'Lorem ipsum dolor sit amet';
-  const description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum gravida ipsum ac augue pharetra, sed vehicula libero ornare. Cras et tristique dolor. Etiam scelerisque leo eu turpis accumsan placerat. Morbi aliquet felis vitae lectus dapibus iaculis. Quisque at placerat nisl. Morbi tincidunt elementum lacus vel interdum. Proin congue vulputate semper. Suspendisse potenti. Fusce ullamcorper ipsum quis ornare pulvinar. Nunc id sem tempor, posuere leo ut, maximus massa. Nullam interdum nunc at ex tempus ornare. Morbi ullamcorper, erat eget imperdiet vehicula, sapien felis scelerisque nulla, at congue mauris nibh a est. Pellentesque id fermentum urna, at feugiat dui. Nunc ut dolor sodales, sollicitudin ligula et, rutrum dolor. Quisque semper nisi enim. Sed semper pretium mauris tincidunt fermentum.';
-  const testimonials = {
-    cedric: {
-      user_identity: 'Cédric',
-      user_testimonial: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum gravida ipsum ac augue pharetra, sed vehicula libero ornare. Cras et tristique dolor. Etiam scelerisque leo eu turpis accumsan placerat.',
-      user_picture: cedric,
-    },
-    aude: {
-      user_identity: 'Aude',
-      user_testimonial: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum gravida ipsum ac augue pharetra, sed vehicula libero ornare. Cras et tristique dolor. Etiam scelerisque leo eu turpis accumsan placerat.',
-      user_picture: aude,
-    },
-    nadia: {
-      user_identity: 'Nadia',
-      user_testimonial: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum gravida ipsum ac augue pharetra, sed vehicula libero ornare. Cras et tristique dolor. Etiam scelerisque leo eu turpis accumsan placerat.',
-      user_picture: nadia,
-    },
-  };
-
-  return (
-    <>
-      <HomeNavbar />
-      <Carousel />
-      <StatisticsBandeau />
-      <PrimaryCTABandeau
-        title={title}
-        description={title}
-        CTA="Meet my Team Now !"
-        icon=""
-      />
-      <FeatureBandeau
-        title={title}
-        description={description}
-        image={image}
-        imageFirst
-      />
-      <FeatureBandeau
-        title={title}
-        description={description}
-        image={image}
-        imageFirst={false}
-      />
-      <PrimaryCTABandeau
-        title={title}
-        description={title}
-        CTA="Meet my Team Now !"
-        icon=""
-      />
-      <Testimonials testimonials={testimonials} />
-      <Footer />
-    </>
-  );
-};
+const Home = () => (
+  <>
+    <HomeNavbar />
+    <Carousel />
+    <StatisticsBandeau />
+    <PrimaryCTABandeau
+      title={content.title}
+      description={content.description}
+      CTA="Meet my Team Now !"
+      icon=""
+    />
+    <FeatureBandeau
+      title={content.title}
+      description={content.description}
+      image={content.footballImage}
+      imageFirst
+    />
+    <FeatureBandeau
+      title={content.title}
+      description={content.description}
+      image={content.footballImage}
+      imageFirst={false}
+    />
+    <PrimaryCTABandeau
+      title={content.title}
+      description={content.description}
+      CTA="Meet my Team Now !"
+      icon=""
+    />
+    <Testimonials testimonials={content.testimonials} />
+    <Footer />
+  </>
+);
 
 export default Home;
