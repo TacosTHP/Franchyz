a5e87d529d6f56c4063c0be3fa1eb22f59daeb19
Dim <dimitri.kiavue@gmail.com>
2020-09-16 19:36:21 +0200

    [UPDATE] PrimaryButton hover

--- src/components/Buttons/PrimaryButton.jsx
+++ src/components/Buttons/PrimaryButton.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import 'styles/button.scss';
+
 const PrimaryButton = ({ text, icon }) => (
   <>
-    <button type="button" className="btn btn-primary text-white font-weight-bold d-flex justify-content-center">
+    <button id="primary-button" type="button" className="btn btn-primary text-white font-weight-bold d-flex justify-content-center">
       <div>
         {icon}
       </div>
