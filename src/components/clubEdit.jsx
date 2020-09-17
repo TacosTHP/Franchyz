5d652dec4265e3bc39738182ec2cbac4d0d8ba8a
floury-fabio <floury-fabio@hotmail.fr>
2020-09-17 15:27:02 +0200

    clean editClub

--- src/components/clubEdit.jsx
+++ src/components/clubEdit.jsx
@@ -1,104 +1,114 @@
-import React, { useState, useEffect } from 'react';
-import * as clubAPI from 'services/clubAPI';
-import { Col, Row } from 'antd';
-import { message} from 'antd';
-import { useHistory } from "react-router-dom";
+import React from 'react';
+import PropTypes from 'prop-types';
+import useInputChange from 'customHooks/useInputChange';
 
-const ClubEdit = ({club}) => {
-  const clubId = club.id;
-  let history = useHistory();
-  const [data, setData] = useState([]);
-
-
-
-
-  useEffect(() => {getData()}, []);
-
-  async function getData() {
-    await setData({
-      name: club.name,
-      date_of_creation: club.date_of_creation,
-      description: club.description,
-      league: club.league,
-      pool: club.pool,
-      conference: club.conference,
-      address: club.address,
-      zip_code: club.zip_code,
-      city: club.city,
-      country: club.country,
-    });
-  };
+const ClubEdit = ({ club }) => {
+  const [input, handleInputChange] = useInputChange({
+    name: club.name,
+    date_of_creation: club.date_of_creation,
+    description: club.description,
+    league: club.league,
+    pool: club.pool,
+    conference: club.conference,
+    address: club.address,
+    city: club.city,
+    country: club.country,
+    zip_code: club.zip_code,
+  });
 
   const submit = (event) => {
-    event.preventDefault();
-    clubAPI.editClub({
-      id: clubId,
-      fields: {data},
-    })
-    .then(() =>  message.success('You edited successfully your club', 2.5))
-  
   };
-
-  let test = '';
 
   return (
     <form onSubmit={submit}>
-      <small>{test}</small>
-      <Row>
-        <Col style={{marginLeft: '2em'}}>
-          <h6 className='font-weight-bold'>Club details:</h6>
+      <div className="container row">
+        <div className="col">
+          <h6 className="font-weight-bold">Club details:</h6>
           <div>
-          <div>
-              <label>Club name:</label>
-              <input type='text' placeholder={(data.name === null) ? 'Club name' : null } value={(data.name === null) ? '' : data.name} onChange={e => setData({...data, name: e.target.value })} />
-            </div>
-            <div>
-              <label>Founded in:</label>
-              <input  type='date' placeholder={(data.date_of_creation === null) ? 'Date of creation' : null } value={(data.date_of_creation === null) ? '' : data.date_of_creation} onChange={e => setData({...data, date_of_creation: e.target.value })} />
-            </div>
-            <div>
-              <label>Club description:</label>
-              <textarea rows='1' placeholder={(data.description === null) ? 'Description' : null } value={(data.description === null) ? '' : data.description} onChange={e => setData({...data, description: e.target.value })} />
-            </div>
-            <div>
-              <label>League:</label>
-              <input type='text' placeholder={(data.league === null) ? 'League' : null } value={(data.league === null) ? '' : data.league} onChange={e => setData({...data, league: e.target.value })} />
-            </div>
-            <div>
-              <label>Pool:</label>
-              <input type='text' placeholder={(data.pool === null) ? 'Pool' : null } value={(data.pool === null) ? '' : data.pool} onChange={e => setData({...data, pool: e.target.value })} />
-            </div>
-            <div>
-              <label>Conference:</label>
-              <input type='text' placeholder={(data.conference === null) ? 'Conference' : null } value={(data.conference === null) ? '' : data.conference} onChange={e => setData({...data, conference: e.target.value })} />
-            </div>
+            <label htmlFor="name">
+              Club name:
+              <input id="name" name="name" type="text" placeholder={input.name} value={input.name} onChange={handleInputChange} />
+            </label>
           </div>
-        </Col>
-        <Col style={{marginLeft: '2em'}}>
           <div>
-            <h6 className='font-weight-bold'>Club address:</h6>
-            <div>
-              <label>Address:</label>
-              <input type='text' placeholder={(data.address === null) ? 'Address' : null } value={(data.address === null) ? '' : data.address} onChange={e => setData({...data, address: e.target.value })} />
-            </div>
-            <div>
-              <label>Zip code:</label>
-              <input type='text' placeholder={(data.zip_code === null) ? 'Zip code' : null } value={(data.zip_code === null) ? '' : data.zip_code} onChange={e => setData({...data, zip_code: e.target.value })} />
-            </div>
-            <div>
-              <label>City:</label>
-              <input type='text' placeholder={(data.city === null) ? 'City' : null } value={(data.city === null) ? '' : data.city} onChange={e => setData({...data, city: e.target.value })} />
-            </div>
-            <div>
-              <label>Country:</label>
-              <input type='text' placeholder={(data.country === null) ? 'Country' : null } value={(data.country === null) ? '' : data.country} onChange={e => setData({...data, country: e.target.value })} />
-            </div>
+            <label htmlFor="date">
+              Founded in:
+              <input id="date" name="date_of_creation" type="date" placeholder={input.date_of_creation} value={input.date_of_creation} onChange={handleInputChange} />
+            </label>
           </div>
-        </Col>
-      </Row>
-      <button type='submit' className='btn btn-primary'>Save</button>
+          <div>
+            <label htmlFor="description">
+              Club description:
+              <textarea id="description" name="description" rows="1" placeholder={input.description} value={input.description} onChange={handleInputChange} />
+            </label>
+          </div>
+          <div>
+            <label htmlFor="league">
+              League:
+              <input id="league" name="league" type="text" placeholder={input.league} value={input.league} onChange={handleInputChange} />
+            </label>
+          </div>
+          <div>
+            <label htmlFor="pool">
+              Pool:
+              <input id="pool" name="pool" type="text" placeholder={input.pool} value={input.pool} onChange={handleInputChange} />
+            </label>
+          </div>
+          <div>
+            <label htmlFor="conference">
+              Conference:
+              <input id="conference" name="conference" type="text" placeholder={input.conference} value={input.conference} onChange={handleInputChange} />
+            </label>
+          </div>
+        </div>
+        <div className="col">
+          <h6 className="font-weight-bold">Club address:</h6>
+          <div>
+            <label htmlFor="address">
+              Address:
+              <input id="address" name="address" type="text" placeholder={input.address} value={input.address} onChange={handleInputChange} />
+            </label>
+          </div>
+          <div>
+            <label htmlFor="zipCode">
+              Zip code:
+              <input id="zipCode" name="zip_code" type="text" placeholder={input.zip_code} value={input.zip_code} onChange={handleInputChange} />
+            </label>
+          </div>
+          <div>
+            <label htmlFor="city">
+              City:
+              <input id="city" name="city" type="text" placeholder={input.city} value={input.city} onChange={handleInputChange} />
+            </label>
+          </div>
+          <div>
+            <label htmlFor="country">
+              Country:
+              <input id="country" name="country" type="text" placeholder={input.country} value={input.country} onChange={handleInputChange} />
+            </label>
+          </div>
+        </div>
+      </div>
+      <button type="submit" className="btn btn-primary">Save</button>
     </form>
   );
 };
 
 export default ClubEdit;
+
+ClubEdit.propTypes = {
+  club: PropTypes.shape({
+    id: PropTypes.number,
+    logo_url: PropTypes.string,
+    name: PropTypes.string,
+    date_of_creation: PropTypes.string,
+    description: PropTypes.string,
+    league: PropTypes.string,
+    pool: PropTypes.string,
+    conference: PropTypes.string,
+    address: PropTypes.string,
+    zip_code: PropTypes.string,
+    city: PropTypes.string,
+    country: PropTypes.string,
+  }).isRequired,
+};
