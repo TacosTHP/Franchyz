d2bbb133b6e11a11f3cd70a67586fa6dec72b6fd
Dim <dimitri.kiavue@gmail.com>
2020-09-17 12:34:01 +0200

    [ADD] Url prop on Buttons

--- src/components/Buttons/DarkButton.jsx
+++ src/components/Buttons/DarkButton.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import 'styles/button.scss';
 
-const DarkButton = ({ text, icon }) => (
+const DarkButton = ({ text, icon, url }) => (
   <>
-    <button type="button" id="dark-button" className="btn btn-dark d-flex justify-content-around align-items-center text-white font-weight-bold">
-      <div className="mr-2" id="icon">
-        {icon}
-      </div>
-      <div>
-        {text}
-      </div>
-    </button>
+    <Link to={url}>
+      <button type="button" id="dark-button" className="btn btn-dark d-flex justify-content-around align-items-center text-white font-weight-bold">
+        <div className="mr-2" id="icon">
+          {icon}
+        </div>
+        <div>
+          {text}
+        </div>
+      </button>
+    </Link>
   </>
 );
 
--- src/components/Buttons/PrimaryButton.jsx
+++ src/components/Buttons/PrimaryButton.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import 'styles/button.scss';
 
-const PrimaryButton = ({ text, icon }) => (
+const PrimaryButton = ({ text, icon, url }) => (
   <>
-    <button id="primary-button" type="button" className="btn btn-primary d-flex align-items-center justify-content-around text-white font-weight-bold mx-auto">
-      <div className="mr-2" id="icon">
-        {icon}
-      </div>
-      <div>
-        {text}
-      </div>
-    </button>
+    <Link to={url}>
+      <button id="primary-button" type="button" className="btn btn-primary d-flex align-items-center justify-content-around text-white font-weight-bold mx-auto">
+        <div className="mr-2" id="icon">
+          {icon}
+        </div>
+        <div>
+          {text}
+        </div>
+      </button>
+    </Link>
   </>
 );
 
@@ -21,4 +24,5 @@ export default PrimaryButton;
 PrimaryButton.propTypes = {
   text: PropTypes.string,
   icon: PropTypes.elementType,
+  url: PropTypes.string,
 }.isRequired;
