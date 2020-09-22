e1e8270e6978cefb6c6c5dfeb417236c67d23cc6
floury-fabio <floury-fabio@hotmail.fr>
2020-09-22 13:40:12 +0200

    cleam team jsx

--- src/components/team.jsx
+++ src/components/team.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-const Team = ({team}) => {
-
-  return (
-    <li className='list-group-item'> <Link to={`/clubs/${team.club_id}/teams/${team.id}`}> {team.title} </Link> </li>
-  );
-}
+const Team = ({ team }) => (
+  <li className="list-group-item">
+    <Link to={`/clubs/${team.club_id}/teams/${team.id}`}>
+      {team.title}
+    </Link>
+  </li>
+);
 
 export default Team;
 
+Team.propTypes = {
+  team: PropTypes.shape({
+    id: PropTypes.number,
+    club_id: PropTypes.number,
+    title: PropTypes.string,
+  }).isRequired,
+};
