41874d0da2d2307f1d9b4252a6879f04ccf745d1
floury-fabio <floury-fabio@hotmail.fr>
2020-09-29 17:34:46 +0200

    add CreationTeamCard

--- src/components/TeamList.jsx
+++ src/components/TeamList.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import InvitationToCreateTeam from 'components/InvitationToCreateTeam';
 import TeamCard from 'components/TeamCard';
+import CreationTeamCard from 'components/CreationTeamCard';
 
 const TeamList = ({ teams }) => {
   if (teams === undefined) {
-    console.log('eoijdfheiu')
     return (<> </>);
   }
 
@@ -16,6 +16,7 @@ const TeamList = ({ teams }) => {
   return (
     <div id="team-list" className="d-flex py-2 border">
       { teams.map((team) => <TeamCard key={team.id} team={team} />) }
+      <CreationTeamCard />
     </div>
   );
 };
