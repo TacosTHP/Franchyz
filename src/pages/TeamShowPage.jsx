03a14ef22a7b8189a60a72b17ebea12f8c9ee4cf
Dim <dimitri.kiavue@gmail.com>
2020-10-14 18:19:31 +0200

    [UPDATE] Add CoachCard on TeamShowPage

--- src/pages/TeamShowPage.jsx
+++ src/pages/TeamShowPage.jsx
@@ -1,17 +1,24 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 import * as teamAPI from 'services/teamAPI';
 
 import Loading from 'components/Loading';
 import CoachCard from 'components/CoachCard';
+import CoachDashboardNavbar from 'components/layouts/CoachDashboardNavbar';
 import PlayersTable from 'components/PlayersTable';
+import LeftArrowIcon from 'components/Icons/LeftArrowIcon';
 
 const TeamShowPage = () => {
   const { clubId, teamId } = useParams();
   const [team, setTeam] = useState();
+  const history = useHistory();
   const loading = useSelector((state) => state.authReducer.loading);
+
+  const goToDashboard = () => {
+    history.push('/dashboardAdmin');
+  };
 
   useEffect(() => {
     const loadData = async () => {
@@ -27,15 +34,31 @@ const TeamShowPage = () => {
 
   if (team !== undefined) {
     return (
-      <div className="layoutPage">
-        <div className="mr-auto text-primary">FLECHE RETOUR</div>
-        <div className="d-flex justify-content-center align-items-center">
-          <h1 className="text-primary text-center">
-            {team.title}
-          </h1>
+      <div className="black-background">
+        <CoachDashboardNavbar club />
+        <div className="p-3">
+          <div className="mr-auto text-primary" onClick={goToDashboard}><LeftArrowIcon size="2em" /></div>
+          <div className="d-flex justify-content-center align-items-center">
+            <h1 className="text-primary text-center">
+              {team.title}
+            </h1>
+          </div>
+          <div className="d-flex">
+            <div className="w-25 mr-2">
+              <h3 className="text-primary">
+                Team Coach
+              </h3>
+              <CoachCard coach={team.coach} />
+              <h3 className="text-white">
+                General Coach
+              </h3>
+              <CoachCard coach={team.coach} />
+            </div>
+            <div className="w-100">
+              <PlayersTable players={team.players} />
+            </div>
+          </div>
         </div>
-        <CoachCard coach={team.coach} />
-        <PlayersTable players={team.players} />
       </div>
     );
   }
